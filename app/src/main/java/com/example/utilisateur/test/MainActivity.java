e0e6fabfb8c62dfbada4f7404da465e3719fd826
Pedro Scoccimarro <pedroscocci@oulook.com>
2019-02-01 09:32:18 -0500

    Test Bundle

--- app/src/main/java/com/example/utilisateur/test/MainActivity.java
+++ app/src/main/java/com/example/utilisateur/test/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //ALOJGOUFDSPHGIUDSHFOIURSAFIUYGAfgdjhyfduhfdghytd
         //La version est change
+        // Test bundle
     }
 }
